c0ffb1ad8fdad1c7564cad086ffa72dc064e898f
Sennikova Alexandra <SennikovaAlexandra01@yandex.ru>
2020-06-25 21:15:31 +0300

    lesson 14 new

--- scriptH.js
+++ scriptH.js
@@ -4,21 +4,36 @@ const article = document.querySelector('article');
 
 const DomElement = {
     selector: '#class',
-    height: 10,
+    height: 80,
     width: 1000,
     bg: 'red',
     fontSize: 56,
     createElement: function() {
+      let text = prompt('введите текст для элемента');
          if(this.selector[0] == '.') {
-            let div = '<div class = ' + this.selector.slice(1) + ', style= "color: ' + this.bg + '; width: ' + this.width + 'px; height: ' + this.height + 'px; fontSize: ' + this.fontSize + 'px"> hello, i am a new element </div>';
-            article.insertAdjacentHTML('afterBegin', div);
+            let div = document.createElement('div');
+            div.textContent = text;
+            div.classListAdd = this.selector.slice(1);
+            div.style.cssText = 'color:' + this.bg + '; background-color: black; width: ' + this.width + 'px; height: ' + this.height + 'px; font-size: ' + this.fontSize + 'px;';
+            
+            article.appendChild(div);
+
+            // let div = '<div class = ' + this.selector.slice(1) + ', style= "color: ' + this.bg + '; width: ' + this.width + 'px; height: ' + this.height + 'px; fontSize: ' + this.fontSize + 'px"> hello, i am a new element </div>';
+            // article.insertAdjacentHTML('afterBegin', div);
             
 
 
          } else if(this.selector[0] == '#') {
-            let p = '<p id = ' + this.selector.slice(1) + '' + ' , style= "color: ' + this.bg + '; width: ' + this.width + 'px; height: ' + this.height + 'px; fontSize: ' + this.fontSize + 'px"> hello, i am a new element </p>';
+            let p = document.createElement('p');
+            p.textContent = text;
+            p.id = this.selector.slice(1);
+            p.style.cssText = 'color:' + this.bg + '; background-color: black; width: ' + this.width + 'px; height: ' + this.height + 'px; font-size: ' + this.fontSize + 'px;';
 
-            article.insertAdjacentHTML('afterBegin', p);
+            article.appendChild(p);
+
+            // let p = '<p id = ' + this.selector.slice(1) + '' + ' , style= "color: ' + this.bg + '; width: ' + this.width + 'px; height: ' + this.height + 'px; fontSize: ' + this.fontSize + 'px"> hello, i am a new element </p>';
+
+            // article.insertAdjacentHTML('afterBegin', p);
 
          }
     }
