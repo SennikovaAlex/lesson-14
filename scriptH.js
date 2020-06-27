1137cdc3228d9506a9ef7247cc4e30274e24d83a
Sennikova Alexandra <SennikovaAlexandra01@yandex.ru>
2020-06-27 11:28:51 +0300

    ref

--- scriptH.js
+++ scriptH.js
@@ -1,117 +1,87 @@
 'use strict';
 
-const article = document.querySelector('article');
+const DomElement = function (selector, height, width, position = 'relative', shift = 10, top = 150) {
+   this.container = document.querySelector('article');
+   this.elemTypeDict = {'.': ['div', 'class'], '#': ['p', 'id']};
+   this.bg = 'background: white;';
+   this.fontSize = 'font-size: 14px; ';
+   this.left = 0;
 
-const DomElement = function () {
-    this.selector = '.class',
-    this.height = 10,
-    this.width = 10,
-    this.bg = 'white',
-    this.fontSize = 14;
+   this.element = document.createElement(this.elemTypeDict[selector[0]][0]);
+   this.selector = selector;
+   this.selectorName = selector.slice(1);
+   this.position = 'position: ' + position + ';';
+   this.shift = shift;
+   this.top = top;
+   this.height = 'height:' + height + 'px; ';
+   this.width = 'width: ' + width + 'px; ';
+   this.elementDom;
+   
+   this.setStyle = function() {
+      this.element.style.cssText = this.bg + this.width + this.height + this.fontSize + this.position +
+      'top: ' + this.top + 'px; ' + 'left: ' + this.left + 'px; ';
+   };
+
+   this.render = function() {
+      this.setStyle();
+      this.element.setAttribute(this.elemTypeDict[selector[0]][1], this.selectorName);
+      this.container.appendChild(this.element);
+      this.getElem();
+   };
+
+   this.getElem = function() {
+      this.elementDom = document.querySelector(this.selector);
+   }
+
+   this.moveDown = function() {
+      console.log(this.elementDom.style.top);   
+      //this.top += 10;
+      //this.elementDom.style.top = this.top + 'px; ';
+   };
 };
-
-DomElement.prototype.style = function() {
-   return 'color:' + this.bg + '; width: ' + this.width + 'px; height: ' + this.height + 'px; font-size: ' + this.fontSize + 'px;' ;
-  };
-
-DomElement.prototype.createElement = function(text) {
-
-      let block;
-         if(this.selector[0] == '.') {
-             block  = document.createElement('div');
-            block.classList.add(this.selector.slice(1));
-            console.log(block);
-
-         } else if(this.selector[0] == '#') {
-             block = document.createElement('p');
-             block.id = this.selector.slice(1);
-            article.appendChild(block);
-         }
-      let textContent;
-       if (text) {
-         textContent = prompt('введите текст для элемента'); 
-         block.textContent = textContent;
-      }
-         article.appendChild(block);
-         
-         block.style.cssText = this.style();
-    };
-
-
-let newElement = new DomElement();
-console.log(newElement);
-newElement.selector = '.text';
-newElement.height = 60;
-newElement.width = 1000;
-newElement.bg = 'red';
-newElement.fontSize = 54;
-
-newElement.createElement(1);
-
 
 document.addEventListener('DOMContentLoaded', function(){
 
-   let newElementSquare = new DomElement();
-   newElementSquare.selector = '.square';
-   newElementSquare.height = 100;
-   newElementSquare.width = 100;     
+   let newElementSquare = new DomElement('.square', 100, 100, 'absolute');   
+   newElementSquare.render();
    
-   newElementSquare.createElement();
-
-    let square = document.querySelector('.square');
-    console.log(square);
-    let topchik = 150;
-    let shift = 10;
-
-    let squareStyle = newElementSquare.style() + ' position: absolute; background-color: red; '; 
-
-   square.style.cssText = squareStyle + ' top: ' + topchik + 'px; left: ' + shift + 'px' ;
-
-   console.log(square.style.cssText);
-   
-   let keyDown = function () { 
+   // let keyUp = function () { 
       
-      topchik += 10;
-      let newSquareStyle = squareStyle + ' top: ' + topchik + 'px; left: ' + shift + 'px'  ;
-      square.style.cssText = newSquareStyle ;
-      // console.log(square.style.cssText);
-   };
+   //    topchik -= 10;
+   //    let newSquareStyle = squareStyle + ' top: ' + topchik + 'px; left: ' + shift + 'px' ;
+   //    square.style.cssText = newSquareStyle ;
+   //    console.log(square.style.cssText);
+   // };
    
-   let keyUp = function () { 
+   // let keyRight = function () { 
       
-      topchik -= 10;
-      let newSquareStyle = squareStyle + ' top: ' + topchik + 'px; left: ' + shift + 'px' ;
-      square.style.cssText = newSquareStyle ;
-      console.log(square.style.cssText);
-   };
+   //    shift += 10;
+   //    let newSquareStyle = squareStyle + ' top: ' + topchik + 'px; left: ' + shift + 'px' ;
+   //    square.style.cssText = newSquareStyle ;
+   //    console.log(square.style.cssText);
    
-   let keyRight = function () { 
-      
-      shift += 10;
-      let newSquareStyle = squareStyle + ' top: ' + topchik + 'px; left: ' + shift + 'px' ;
-      square.style.cssText = newSquareStyle ;
-      console.log(square.style.cssText);
+   // };
    
-   };
+   // let keyLeft = function () { 
+   //    shift -= 10;
+   //    let newSquareStyle = squareStyle + ' top: ' + topchik + 'px; left: ' + shift + 'px' ;
+   //    square.style.cssText = newSquareStyle ;
+   //    console.log(square.style.cssText);
    
-   let keyLeft = function () { 
-      shift -= 10;
-      let newSquareStyle = squareStyle + ' top: ' + topchik + 'px; left: ' + shift + 'px' ;
-      square.style.cssText = newSquareStyle ;
-      console.log(square.style.cssText);
-   
-   };
+   // };
    
    document.addEventListener('keydown', function() {
-      if (event.code == 'ArrowDown') {
-         keyDown();    
-      } else if (event.code == 'ArrowUp') {
-         keyUp();
-      } else if (event.code == 'ArrowRight') {
-         keyRight();
-      } else if (event.code == 'ArrowLeft') {
-         keyLeft();
-      }
+      newElementSquare.moveDown();     
+
+      // if (event.code == 'ArrowDown') {
+      //    keyDown();    
+      // } else if (event.code == 'ArrowUp') {
+      //    keyUp();
+      // } else if (event.code == 'ArrowRight') {
+      //    keyRight();
+      // } else if (event.code == 'ArrowLeft') {
+      //    keyLeft();
+      // }
    
    });
 } );
